cde4011324cede71defc698a2af9b455b0a9193c
Rey Blackburn <rey.blackburn@revature.net>
2021-10-13 14:44:36 -0600

    removed Javalin from launcher

--- Project2/src/main/java/com/revature/Launcher.java
+++ Project2/src/main/java/com/revature/Launcher.java
@@ -1,14 +1,9 @@
 package com.revature;
 
-import static io.javalin.apibuilder.ApiBuilder.*;
-
-
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
 import com.revature.utils.HibernateUtil;
-
-import io.javalin.Javalin;
 
 public class Launcher {
 
@@ -16,7 +11,7 @@ public class Launcher {
 
 		try(Session ses = HibernateUtil.getSession()){
 			System.out.println("Hello you have a Connection to your DB with Hibernate!");
-			HibernateUtil.closeSession(); //if you want to leave this connection test in, close the session within the try
+			HibernateUtil.closeSession();
 		} catch (HibernateException e) {
 			System.out.println("DB connection failed!!");
 			e.printStackTrace();
