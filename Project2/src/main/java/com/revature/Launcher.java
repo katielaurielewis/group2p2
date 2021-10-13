29059889e558560456299ed7ec6b8c7cb5643654
Rey Blackburn <rey.blackburn@revature.net>
2021-10-13 10:30:23 -0600

    Updated classes for hibernate link updated launcher to test for connection

--- Project2/src/main/java/com/revature/Launcher.java
+++ Project2/src/main/java/com/revature/Launcher.java
@@ -1,9 +1,21 @@
 package com.revature;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import com.revature.utils.HibernateUtil;
+
 public class Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		try(Session ses = HibernateUtil.getSession()){
+			System.out.println("Hello you have a Connection to your DB with Hibernate!");
+			HibernateUtil.closeSession(); //if you want to leave this connection test in, close the session within the try
+		} catch (HibernateException e) {
+			System.out.println("DB connection failed!!");
+			e.printStackTrace();
+		}
 
 	}
 
