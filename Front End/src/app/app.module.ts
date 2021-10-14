2846e46deab4c2fce8cf8bdadfa60d8247bb0b5e
Kuhdac <87532020+Kuhdac@users.noreply.github.com>
2021-10-14 14:59:37 -0400

    restructured front end file structure

--- Front End/src/app/app.module.ts
+++ Front End/src/app/app.module.ts
@@ -4,34 +4,27 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LibraryComponent } from './components/library/library.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegistrationComponent } from './components/registration/registration.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { WatchListComponent } from './components/watch-list/watch-list.component';
-import { RecommenderComponent } from './components/recommender/recommender.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { RecommenderComponent } from './features/recommender/recommender.component';
+import { LibraryComponent } from './features/library/library.component';
+import { UserDashboardComponent } from './features/user-dashboard/user-dashboard.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LibraryComponent,
-    LoginComponent,
-    RegistrationComponent,
     NavbarComponent,
-    WatchListComponent,
     RecommenderComponent,
-    DashboardComponent
+    LibraryComponent,
+    UserDashboardComponent,
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule,
-    CollapseModule.forRoot(),
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
