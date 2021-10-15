52690ea8e03ec34a18eefc333ef49300058fbe1f
Katie Lewis <katielaurielewis@gmail.com>
2021-10-14 22:12:47 -0400

    file structure again

--- Project2/src/main/java/com/revature/daos/ReviewDaoInterface.java
+++
@@ -1,9 +0,0 @@
-package com.revature.daos;
-
-import com.revature.models.Review;
-
-public interface ReviewDaoInterface {
-
-	void addReview(Review review);
-
-}
--- Project2/src/main/java/com/revature/daos/StudioDaoInterface.java
+++
@@ -1,9 +0,0 @@
-package com.revature.daos;
-
-import com.revature.models.Studio;
-
-public interface StudioDaoInterface {
-
-	void addStudio(Studio studio);
-
-}
