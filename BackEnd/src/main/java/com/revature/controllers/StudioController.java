ad27bd87d10cb9d447a3513a12a0576e3c95a9e9
benwbackstrom <benwbackstrom@gmail.com>
2021-10-20 21:44:04 -0500

    Reverted files readded

---
+++ BackEnd/src/main/java/com/revature/controllers/StudioController.java
@@ -0,0 +1,49 @@
+package com.revature.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.models.Studio;
+import com.revature.services.StudioService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials ="true")
+@RequestMapping(value="/studio")
+public class StudioController {
+
+	private StudioService sService;
+
+
+	@Autowired
+	public StudioController(StudioService sService) {
+		super();
+		this.sService = sService;
+	}
+
+	@GetMapping(value = "/name/{name}")  
+	public ResponseEntity<List<Studio>> findByName(@PathVariable String name) {
+
+
+
+		Optional<List<Studio>> opt = sService.findByName(name);
+
+
+		List<Studio> studioList = null;
+
+		if(opt.isPresent()) { 
+			studioList = opt.get(); 
+		}
+
+		return ResponseEntity.ok(studioList);
+
+	}
+
+}
--- BackEnd/src/main/java/com/revature/controllers/UserController.java
+++ BackEnd/src/main/java/com/revature/controllers/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +38,15 @@ public class UserController {
 		
 		return ResponseEntity.ok(u);
 		
+	}
+	
+	@GetMapping(value = "/user/{username}")  
+	public ResponseEntity<User> findByUserName(@PathVariable String username) {
+
+		User user = uService.findByUsername(username);
+
+		return ResponseEntity.ok(user);
+
 	}
 
 }
